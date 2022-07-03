81b6720dc1ecf9807f5414193acaf2a055543ad2
Ron Kahat <ron.kahat@celsius.network>
2022-07-03 21:53:51 +0300

    add top bar msg (ipfs link)

---
+++ scripts/top-text-notify.js
@@ -0,0 +1,20 @@
+function showNotificationBar(message, duration, bgColor, txtColor, height) {
+
+    duration = typeof duration !== 'undefined' ? duration : 12000;
+    bgColor = typeof bgColor !== 'undefined' ? bgColor : "#A97AB8";
+    txtColor = typeof txtColor !== 'undefined' ? txtColor : "#100A0A";
+    height = typeof height !== 'undefined' ? height : 30;
+
+    if ($('#notification-bar').length == 0) {
+        var HTMLmessage = "<div class='notification-message' style='text-align:center; line-height: " + height + "px;'> " + message + " </div>";
+        $('body').prepend("<div id='notification-bar' style='display:none; width:100%; height:" + height + "px; background-color: " + bgColor + "; position: fixed; top: 0px; font-size: 16px; z-index: 100; color: " + txtColor + ";border-bottom: 1px solid " + txtColor + ";'>" + HTMLmessage + "</div>");
+    }
+
+    $('#notification-bar').slideDown(function() {
+        setTimeout(function() {
+            $('#notification-bar').slideUp(function() {});
+        }, duration);
+    });
+}
+
+showNotificationBar('This website also hosted on IPFS at <a href="http://ronkahat.eth/" target="_blank">ronkahat.eth</a>');
